3674fd4b51b029628a0b8e749beea9dfbcc61893
Vintage <167544574+Vintaq@users.noreply.github.com>
2024-04-19 18:52:37 +0300

    Create startGame.hpp

---
+++ src /desk_functions/startGame.hpp
@@ -0,0 +1,22 @@
+
+/*
+ * Battleship, classic battleship game
+ * Copyright (C) 2014-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef START_GAME_HPP_
+#define START_GAME_HPP_
+
+#include "Game.hpp"
+
+/** Create all objects necessary for every type of game.
+\param window MainWindow
+\param width Width of game board
+\param length Length of game board
+*/
+QSharedPointer<Game> startGame(QObject* window,
+                               int width, int length);
+
+#endif
